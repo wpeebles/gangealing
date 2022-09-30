7821624542192a98abac73802e96b8727d517c3e
codyreading <codyreading@gmail.com>
2022-09-30 15:30:44 -0400

    Added training script

--- scripts/training/celeba.sh
+++ scripts/training/celeba.sh
@@ -2,5 +2,5 @@
 # the --batch argument so total batch size is preserved. Default value of --batch is 5 (assumes 8 gpus for training
 # for a total batch size of 40 across all gpus)
 torchrun --nproc_per_node=8 train.py \
---ckpt celeba --load_G_only --padding_mode border --vis_every 5000 --ckpt_every 50000 \
+--ckpt celeba --load_G_only --padding_mode border --gen_size 128 --vis_every 5000 --ckpt_every 50000 \
 --iter 1500000 --tv_weight 2500 --ndirs 512 --inject 6 --loss_fn lpips --exp-name in_the_wild_celeba
